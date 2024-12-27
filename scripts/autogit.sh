2d00c7d54459b67c462ba4b8fade017b99362365
Mateo A <mateo@Mateos-Air.lan>
2024-12-27 00:18:22 -0800

    commited to repo using autogit

--- scripts/autogit.sh
+++ scripts/autogit.sh
@@ -4,9 +4,6 @@ help_str="help"
 init_str="init"
 deinit_str="remove"
 info_str="info"
-new_str="new"
-switch_str="switch"
-delete_str="delete"
 push_str="push"
 pull_str="pull"
 
@@ -16,149 +13,112 @@ help(){
     echo "init - initialize a local and remote repository and create branch main - autogit init <remote_url_name> \n"
     echo "deinit - remove currently existing remote - autogit deinit \n"
     echo "info - display current information of current remotes and branches - autogit info \n"
-    echo "new - creates a new branch within remote - autogit new <branch_name> \n"
-    echo "switch - changes current working branch - autogit change <branch_name> \n"
-    echo "delete - deletes a given branch from remote - autogit delete <branch_name> \n"
-    echo "push - push all current changes to a specified branch - autogit push <branch_name> <optional: commit message> \n"
+    echo "push - push all current changes to a specified branch - autogit push <branch_name> \n"
     echo "pull - pull all current changes from a specified branch - autogit pull <branch_name> \n\n"
 
     echo "written by matesuu - (December 2024) \n\n"
 }
 
 init(){
+
     git init && git symbolic-ref HEAD refs/heads/main
     git config --global init.defaultBranch main
+
     git remote add origin $2
     git remote -v
 }
 
 deinit(){
+
     git remote remove origin
     git remote -v
 }
 
 info(){
+
     git remote -v
     git branch
-}
-
-new(){
-    git checkout -b $2
-}
-
-change(){
-    git checkout $2
-}
-
-delete(){
-    git branch -d $2
 }
 
 push(){
 
     git status
     git add . --all
-    git commit -m "committed to repo using autogit"
-    git pull --rebase origin $2
-    git push -u origin $2
-}
 
-push_commit(){
-
-    git status
-    git add . --all
-    
-    # Check if commit message exists and use default message if not
-    if [ -z "$3" ]; then
-        echo "No commit message provided, using default."
-        commit_msg="committed to repo using autogit"
-    else
-        commit_msg="$3"
-    fi
-
-    echo "Committing with message: $commit_msg"
-    git commit -m "$commit_msg"
+    git commit -m "commited to repo using autogit"
 
     git pull --rebase origin $2
     git push -u origin $2
 }
 
 pull(){
+
     git status
     git pull origin $2
 }
 
 if [ $# -eq 0 ]; then
+
     help
     exit 0
 fi
 
 if [ "$1" == "$help_str" ]; then
+
     help
 
 elif [ "$1" == "$init_str" ]; then
+
     if [ $# -eq 2 ]; then
+
         init $2
         echo "process finished "
+
     else
+
         echo "missing argument: remote repository url "
         exit 1
     fi
 
 elif [ "$1" == "$deinit_str" ]; then
+
     deinit
     echo "process finished "
 
 elif [ "$1" == "$info_str" ]; then
+
     info
     echo "process finished "
 
-elif [ "$1" == "$new_str" ]; then
-    if [ $# -eq 2 ]; then
-        new $2
-        echo "process finished "
-    else
-        echo "missing argument: target branch name "
-        exit 1
-    fi
-
-elif [ "$1" == "$switch_str" ]; then
-    if [ $# -eq 2 ]; then
-        change $2
-        echo "process finished "
-    else
-        echo "missing argument: target branch name "
-        exit 1
-    fi
-
-elif [ "$1" == "$delete_str" ]; then
-    if [ $# -eq 2 ]; then
-        delete $2
-        echo "process finished "
-    else
-        echo "missing argument: target branch name "
-        exit 1
-    fi
-
 elif [ "$1" == "$push_str" ]; then
-    if [ $# -ge 3 ]; then
-        push_commit $2 "$3"
-        echo "process finished "
-    else
+
+    if [ $# -eq 2 ]; then
+
         push $2
         echo "process finished "
+
+    else
+
+        push
+        echo "missing argument: branch name "
+    
     fi
 
 elif [ "$1" == "$pull_str" ]; then
+
     if [ $# -eq 2 ]; then
+
         pull $2
         echo "process finished "
+
     else
+
         echo "missing argument: branch name "
         exit 1
     fi
 
 else
+
     echo "error: unknown command "
 fi
 
