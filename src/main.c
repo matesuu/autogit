e34cbef5695b91df01746d92ff4c06255498faf8
Mateo A <mateo@Mateos-Air.lan>
2024-12-29 01:13:54 -0800

    commited to repo using autogit

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@ int main(int argc, char** argv)
     char* delete_str = "delete";
     char* push_str = "push";
     char* pull_str = "pull";
+    char* kill_str = "kill";
 
     if(argc < 2)
     {
@@ -143,6 +144,14 @@ int main(int argc, char** argv)
         }
 
         pull(argv[2]);
+        printf("process finished ");
+
+        return 0;
+    }
+
+    else if(strcmp(kill_str, argv[1]) == 0)
+    {
+        kill();
         printf("process finished ");
 
         return 0;
